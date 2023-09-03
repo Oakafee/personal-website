567b585bf432e4ef8684afbf2408d94d434b96a6
Jesse Fried <jefrttt@gmail.com>
2023-09-03 14:11:45 -0400

    Whatever was there before adding GIS stuff

--- src/personal-website.js
+++ src/personal-website.js
@@ -5,44 +5,62 @@ window.onload = function() {
     	works: [
     		{
     			'title': 'ASCAP: Member Enrollment',
-    			'worktype': 'professional',
-    			'img': '',
-    			'description': 'Blah blah blah',
+    			'worktype': 'Professional',
+    			'imgSrc': 'works/ome.png',
+    			'description': 'Jesse wrote all of the markup and CSS for this formidable interactive and specialized form as well as much of the functionality.',
     			'href': 'https://ome.ascap.com/',
     		},
     		{
     			'title': 'ASCAP: Member Access',
-    			'worktype': 'professional',
-    			'description': 'Blah blah blah',
+    			'worktype': 'Professional',
+    			'imgSrc': 'works/ma.png',
+    			'description': 'Jesse added many new features and maintained this massive Backbone app',
     			'href': 'https://www.ascap.com/member-access',
     		},
     		{
     			'title': 'Rebuilding Together: COVID Safety Training',
-    			'worktype': 'professional',
-    			'description': 'Blah blah blah',
+    			'worktype': 'Professional',
+    			'imgSrc': 'works/rtnyc-covid.png',
+    			'description': 'An interactive training and quiz for workers and volunteers about doing construction work during COVID.',
     			'href': 'https://rebuildnyc.org/covid',
     		},
     		{
     			'title': 'Lovers List',
-    			'worktype': 'professional',
-    			'description': 'Blah blah blah',
-    			'href': 'https://ome.ascap.com/',
+    			'worktype': 'Professional',
+    			'imgSrc': 'works/loverslist.png',
+    			'description': 'A freelance project for a writer',
+    			'href': 'http://www.jonfried.net/lovers-list',
     		},
     		{
-    			'title': 'ASCAP: Member Enrollment',
-    			'worktype': 'professional',
-    			'description': 'Blah blah blah',
-    			'href': 'https://ome.ascap.com/',
+    			'title': 'Interpretation of New Jersey',
+    			'worktype': 'Creative',
+    			'imgSrc': 'works/inj.png',
+    			'description': 'An interactive map of New Jersey with articles',
+    			'href': 'https://www.interpretation-of-nj.com/',
     		},
+    		{
+    			'title': 'Development of New Jersey\'s municipalities',
+    			'worktype': 'Creative',
+    			'imgSrc': 'works/nj-muni.png',
+    			'description': 'A visualization of property tax data for NJ by year built',
+    			'href': 'https://nj-muni.s3.us-east-2.amazonaws.com/index.html',
+    		}
     	],
     	renderWorks() {
     		let workEntity = document.getElementsByClassName(this.workEntityClassName)[0];
     		let workList = document.getElementsByClassName(this.workListClassName)[0];
-    		
-    		this.works.forEach(() => {
+
+    		this.works.forEach((workInfo) => {
     			let newWorkEntity = workEntity.cloneNode(true);
+    			newWorkEntity.href = workInfo['href'];
+    			// there has to be a better way than just 1, 2, 3
+    			newWorkEntity.children[0].src = workInfo['imgSrc'];
+				newWorkEntity.children[1].innerText = workInfo['worktype'];
+    			newWorkEntity.children[2].innerText = workInfo['title'];
+    			newWorkEntity.children[3].innerText = workInfo['description'];
 	    		workList.appendChild(newWorkEntity);
     		});
+    		workEntity.style.display = 'none';
 		}
     }
 	personalWebsite.renderWorks();
